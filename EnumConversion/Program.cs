518254ce431566155e918a030600fca0be00f42a
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-28 22:24:18 +0600

    Add project files.

---
+++ EnumConversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnumConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
